561f32c8f1569e57f27c206b57fd49105f5fa56c
mesangue <msanguesa2017@kellogg.northwestern.edu>
2020-05-06 23:31:28 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL=postgresql://postgres:postgres@localhost:5432/agency
+export DATABASE_URL=postgres://sxdlqkgjmhkxmn:56f58f5e13bb58f08767c8b9c0633e979c5a3c9c531699e9b1ded24b29220bf5@ec2-54-165-36-134.compute-1.amazonaws.com:5432/d88pufg7bo1ti0
 export AUTH0_DOMAIN=fsnd-auth-mesangue.auth0.com
 export ALGORITHMS=RS256
 export API_AUDIENCE=agency-api
